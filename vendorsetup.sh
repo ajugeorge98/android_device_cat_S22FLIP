1cacd1c4369ccc109bb172fae70cdd84a470b6de
Aju George <54764698+ajugeorge98@users.noreply.github.com>
2025-05-14 01:37:56 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -62,7 +62,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	    #Magiskboot
 	    export OF_USE_MAGISKBOOT="1"
 	    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
-	    export OF_PATCH_VBMETA_FLAG="1"
+	    export FOX_PATCH_VBMETA_FLAG=1
 
 	    #Magisk
 	    export FOX_USE_SPECIFIC_MAGISK_ZIP="/home/pranav/Magisk/Magisk-v24.3.zip"
